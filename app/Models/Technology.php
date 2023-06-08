f489507be800e8b28d85f2419279c324b4db6f53
FilippoFoti <filippofoti7@gmail.com>
2023-06-08 14:41:32 +0200

    Add: migration & model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -5,16 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Technology extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['title', 'content', 'slug', 'type_id'];
-
-    public function type()
-    {
-        return $this->belongsTo(Type::class);
-    }
 
     public function projects()
     {
